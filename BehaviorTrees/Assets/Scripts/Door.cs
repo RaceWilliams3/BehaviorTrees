04ec1ca11200cf45e6620945b60868737e554751
RaceWilliams3 <willr108@my.erau.edu>
2023-04-09 22:53:04 -0700

    UI now added

--- BehaviorTrees/Assets/Scripts/Door.cs
+++ BehaviorTrees/Assets/Scripts/Door.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
@@ -9,6 +10,20 @@ public class Door : MonoBehaviour
 
     public Vector3 closedRot = new Vector3(0, 0, 0);
     public Vector3 openRot = new Vector3(0, -135, 0);
+
+    public Toggle openTog;
+    public Toggle lockedTog;
+
+    public void UpdateDoor()
+    {
+        open = openTog.isOn;
+        
+    }
+
+    void Update()
+    {
+        locked = lockedTog.isOn;
+    }
 
     void Awake()
     {
